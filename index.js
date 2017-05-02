20c7e6930f37a8b7af02c6b172cd5476019a8227
Streed12 <steve.t.reed@gmail.com>
2017-05-02 05:26:52 -0700

    Prompt setup.

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 #!/usr/bin/env node
 const game = require('commander');
-const userPrompt = require('./prompt.js');
+const prompt = require('prompt');
 
-var board =`
-===============================
-|         |         |         |
-|         |         |         |
-===============================
-|         |         |         |
-|         |         |         |
-===============================
-|         |         |         |
-|         |         |         |
-===============================`;
+const squares = [
+['',' | ' '', ' | ',  ''],
+['',' | ' '', ' | ',  ''],
+['',' | ' '', ' | ',  ''],
+];
+
+let user1;
+let user2;
+
+const createBoard = () => {
+  console.log(squares[0]);
+  console.log(squares[1]);
+  console.log(squares[2]);
+};
 
 
-game
-  .arguments('<file>')
-  .option('-s', userPrompt())
-  .action(function() {
-    console.log(board);
-  })
-.parse(process.argv);
+
+  prompt.start();
+
+  prompt.get(['User1', 'User2'], function (err, result) {
+    if (err) { return onErr(err); }
+    user1 = result.user1;
+    user2 = result.user2;
+    console.log('User info received:');
+    console.log('  User1: ' + result.user1);
+    console.log('  User2: ' + result.user2);
+  });
+
+  function onErr(err) {
+    console.log(err);
+    return 1;
+  }
+
